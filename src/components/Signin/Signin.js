a60979b8e54473f6c96b0c635bbacdc0f043a4c5
Daniel Urbanowicz <urbanowicz.daniel@gmail.com>
2020-01-07 14:40:09 +0100

    Add connection to heroku

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -19,7 +19,7 @@ class Signin extends React.Component {
 
   onSubmitSignIn = (event) => {
     event.preventDefault();
-    fetch('http://localhost:3000/signin', {
+    fetch('https://lit-hollows-69224.herokuapp.com/signin', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
